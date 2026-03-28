b0506fddb42893178f62f965c24c77643d3ce707
lubellomo <lubellomo@hotmail.com>
2026-03-28 12:48:58 -0300

    Arquivo Aula 1
    Tipos de dados primitivos em Python

---
+++ aula1/Tipos_dadis_variaveis.py
@@ -0,0 +1,96 @@
+# dados 
+# scrip - interpretar -  resposta 
+# lógica + melhorar 
+# memória ram - curta duração
+
+# 4 tipos
+
+# cadastro 
+# nome idade email peso altura endereco
+# salario > 2000
+
+# texto   -  "text"  'text'  -  nome  - email - endereco
+# inteiro - 10,  0  ,  -1  -  idade 
+# decimal - 5.2, 9.10,0.  - peso  - altura  - 
+# lógico  - True ,  False  -  saslario
+#            1        0
+
+print(10 + 2)
+print(True + 10)
+print(5.2 + 5)
+print('text' + "fhsdjkfh")
+print('fhsdgfhsdgfs')
+
+
+# concatenação juntar dados
+
+print('Olá', 'João')
+print(f'Olá {'João'}')
+
+# indentação -  organizar o código
+
+print()
+
+
+# variáveis
+
+imc = 0
+
+# Não podem por numero
+# caracteres especiais # @% &*
+
+# pode começar por letra
+# _ underline pode utilizar
+
+# seguida de um igual 
+
+# ESTRUTURA DE DADO
+
+imc = 0 
+altura  =  1.70
+peso = 62.0
+imc =  peso // (altura ** 2)
+print(imc)
+
+# sinais aritméticos 
+# +   -    *   /   %   **  // 
+
+print('SINAIS ARITMÉTICOS')
+# saida sem é um número 
+
+x = 10
+y = 20
+x = 200
+
+print(x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+print(x ** y)
+
+
+print(x // y)
+print(x % y)
+
+
+# sinais lógicos 
+y = 20
+x = 200
+print(x == y) # comparação
+print(x > y) # maior
+print(x < y) # menor
+print(x >= y) # maior ou igual
+print(x <= y) # menor ou igual
+print(x != y) # diferente
+
+
+
+
+
+
+
+
+
+
+
+
